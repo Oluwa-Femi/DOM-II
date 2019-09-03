3c2b5d76d874b73bd50c11afa2e70f4c32ee16ba
Oluwa-femi <oluwatolablessing@gmail.com>
2019-09-03 13:35:16 +0100

    Enlarge logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+//Select fun bus logo
+let logo = document.querySelector(".logo-heading");
+// Add event, enlarge logo on mouse over
+logo.addEventListener("mouseover", event => {
+    logo.style.fontSize = "50px";
+  });
+  addHover(logo);
