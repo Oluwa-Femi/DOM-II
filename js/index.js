7d71b027ad9f34626b0797281539f3aa40235f7f
Oluwa-femi <oluwatolablessing@gmail.com>
2019-09-03 14:57:53 +0100

    Logo enlarged

--- js/index.js
+++ js/index.js
@@ -2,10 +2,9 @@
 //Select elements
 let logo = document.querySelector(".logo-heading");
 
-//Add event: make logo larger on mouseover and addHover
+//Add event: Enlarge logo
 logo.addEventListener("click", event => {
   logo.style.fontSize = "60px";
 });
-addHover(logo);
 
 
